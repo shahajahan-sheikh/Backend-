eae8743e95069ccd01a70689121952d399abe011
Shahajahan Sheikh <87540834+shahajahan-sheikh@users.noreply.github.com>
2024-07-29 13:52:45 +0530

    javabasics

---
+++ javabasics/index.js
@@ -0,0 +1,27 @@
+// Understanding variable, data type , ooperters, looops, conditional stetement, function, etc
+
+// // var a = 12;
+// // var b = 13; 
+// // a = 32;
+// // b = 43;
+
+
+// // primitive and references
+
+// // References = ({[]})
+
+// //  Others all are Primitives value
+
+// // var a = [1,2,3];
+
+// // var b = [...a];
+
+// // b.pop();
+
+// var a = {name: "a", type: "polo"};
+
+
+
+// var b = {...a};
+
+// b.name = "f"; -->
